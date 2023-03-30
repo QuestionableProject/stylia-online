aa767db6ad4113168e21705998a0ca9d855df6eb
dead <ddosattack398@gmail.com>
2023-03-31 00:50:16 +0300

    fix: frontend

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Page from './page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
 );
---
+++ app/src/page.js
@@ -0,0 +1,12 @@
+import Layout from "./layout";
+
+
+function Page() {
+  return (
+    <Layout>
+      <h1>Hello World!</h1>
+    </Layout>
+  );
+}
+
+export default Page;
